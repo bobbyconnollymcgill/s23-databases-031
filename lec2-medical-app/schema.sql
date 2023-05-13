82424f74e100b40bfc108e970e1c7bf82c6158db
Bobby Connolly <thomas.connolly@mcgill.ca>
2023-05-12 21:15:58 -0400

    small delta

--- lec2-medical-app/schema.sql
+++ lec2-medical-app/schema.sql
@@ -1,15 +1,27 @@
 
 -- Create the "Patient Registration" table
 create table patients (
-    id              serial primary key not null,
+    id              serial primary key,
     last_name       varchar(50) not null,
-    other_last_name varchar(50),
     first_name      varchar(50) not null,
-    middle_name     varchar(50),
     birth_date      date not null,
     gender          varchar(20), -- male, female, other, unknown
     health_card     varchar(50),
     email           varchar(255),
+    phone_number    varchar(20),
+    check (birth_date <= CURRENT_TIMESTAMP),
+    check (gender in ('male', 'female', 'non-binary', 'other', 'unknown'))
+);
+
+-- Enforcing unique health_card numbers, but allowing multiple nulls
+CREATE UNIQUE INDEX idx_health_card ON patients (health_card) WHERE health_card IS NOT NULL;
+
+-- 1-to-1* relationship table
+create table patient_other_infos (
+    id              serial primary key,
+    patient_id      integer references patients(id) unique,
+    other_last_name varchar(50),
+    middle_name     varchar(50),
     street          varchar(255),
     apartment       varchar(10),
     city            varchar(50),
@@ -21,17 +33,15 @@ create table patients (
     cell_phone      varchar(20),
     emergency_contact_name  varchar(255),
     emergency_contact_phone varchar(20),
-    check (birth_date <= CURRENT_TIMESTAMP),
-    check (gender in ('male', 'female', 'non-binary', 'other', 'unkown'))
-);
-
--- Enforcing unique health_card numbers, but allowing multiple nulls
-CREATE UNIQUE INDEX idx_health_card ON patients (health_card) WHERE health_card IS NOT NULL;
-
--- 1-to-1 relationship table
-create table patient_other_infos (
-    id              serial primary key not null,
-    patient_id      integer references patients(id) unique,
     sin             varchar(20),
-    notes           varchar(5000)
+    notes           varchar(5000),
+    foreign key (patient_id) references patients (id) -- this doesn't seem to be working
 );
+
+create table medical_billings (
+    id              serial primary key,
+    patient_id      integer references patients(id),
+    invoice_number  varchar(10),
+    balance         numeric(10, 2) default 0.00
+    -- ....
+)
---
+++ lec2-medical-app/scrap.sql
@@ -0,0 +1,8 @@
+insert into patients (last_name, first_name, birth_date, gender, email, phone_number)
+values ('Connolly', 'Smokey', '1994-01-01', 'unknown', 'smokey@smokey.com', '5145552224');
+
+insert into patients (last_name, first_name, birth_date, gender, email, phone_number)
+values ('Connolly', 'Taco', '1992-01-01', 'unknown', 'taco@taco.com', '5145552224');
+
+insert into patients (last_name, first_name, birth_date, gender, email, phone_number)
+values ('Connolly', 'Bobby', '1990-01-01', 'unknown', 'bob@bob.com', '5145552224');
